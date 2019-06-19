fe385227f120aa3930523ce21994cc2e3309b827
Daniel Dąbrowski <elosik123@onet.pl>
2019-06-19 19:07:45 +0200

    Added react

--- server/app.js
+++ server/app.js
@@ -60,7 +60,7 @@ app.use('/graphql', graphqlHttp({
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphqltest-bohk2.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
 	.then(() => {
-		app.listen(process.env.PORT || 3000);
+		app.listen(process.env.PORT || 8080);
 	}).catch(err => {
 		throw err;
 	})
